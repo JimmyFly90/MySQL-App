39aeb7ef4d423963d8c3699d5318cc2137460347
James Flynn <jamesdeclonflynn@hotmail.com>
2024-11-25 14:49:24 +0000

    Making MySQL app with try catch method to check connected to server

---
+++ MySQL-App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using MySqlConnection connection = new("server=localhost;user=root;password=Cadhla24!;");
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Successfully Connected To The MySQL Server");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+    }
+}
