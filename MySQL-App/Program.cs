48964911a17d8f04d2d9fb3a504d6d221ee79759
James Flynn <jamesdeclonflynn@hotmail.com>
2024-12-10 11:00:45 +0000

    Using reader.Read to Select columns from table with condition

--- MySQL-App/Program.cs
+++ MySQL-App/Program.cs
@@ -6,20 +6,6 @@ public class Program
 {
     public static string connectionString = "server=localhost;user=root;password=Cadhla24!;database=db1;";
 
-    public static object[,] groceryItemsData = new object[,]
-    {
-        { "Bananas", "Fruits", 0.99f, 150 },
-        { "Apples", "Fruits", 1.49f, 100 },
-        { "Carrots", "Vegetables", 0.79f, 200 },
-        { "Potatoes", "Vegetables", 1.29f, 180 },
-        { "Milk", "Dairy", 2.49f, 80 },
-        { "Eggs", "Dairy", 1.99f, 120 },
-        { "Bread", "Bakery", 1.99f, 90 },
-        { "Chicken", "Meat", 4.99f, 50 },
-        { "Rice", "Grains", 3.99f, 120 },
-        { "Pasta", "Grains", 1.49f, 150 }
-    };
-
     static void Main(string[] args)
     {
 
@@ -27,22 +13,23 @@ public class Program
         {
             connection.Open();
 
-            string cmdText = "CREATE TABLE store (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), category VARCHAR(100), price FLOAT, stock_quantity INT);";
-            MySqlCommand cmd = new MySqlCommand(cmdText, connection);
-            cmd.ExecuteNonQuery();
+            string commandText = "SELECT id, name, category, price, stock_quantity FROM store WHERE stock_quantity < 100;";
+            MySqlCommand command = new MySqlCommand(commandText, connection);
 
-            for (int i = 0; i < 10; ++i)
+            using (MySqlDataReader reader = command.ExecuteReader())
             {
-                string name = (string)groceryItemsData[i, 0];
-                string category = (string)groceryItemsData[i, 1];
-                float price = (float)groceryItemsData[i, 2];
-                int stock_quantity = (int)groceryItemsData[i, 3];
+                while(reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    string name = reader.GetString("name");
+                    string category = reader.GetString("category");
+                    int price = reader.GetInt32("price");
+                    int quantity = reader.GetInt32("stock_quantity");
 
-                cmdText = $"INSERT INTO store (name, category, price, stock_quantity) VALUES ('{name},', '{category}', '{price}', '{stock_quantity}');";
-                cmd = new MySqlCommand(cmdText, connection);
-                cmd.ExecuteNonQuery();
+                    Console.WriteLine($"ID: {id}, Item Name: {name}, Category: {category}, Price: {price}, Quantity: {quantity}");
+                }
             }
-            
         }
+            
     }
 }
--- MySQL-App/obj/Debug/net8.0/MySQL-App.AssemblyInfo.cs
+++ MySQL-App/obj/Debug/net8.0/MySQL-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySQL-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785d8de92599372017599280aa7331b10f5ac7d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c290dda7b411b002505766722fa65b25820786")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySQL-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySQL-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
